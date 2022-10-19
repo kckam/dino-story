e27d4729c3b3409ac368530244cca3f2fcc0767e
Kok Chin <kamkokchin@gmail.com>
2022-10-19 15:51:44 +0800

    add cache control

--- next.config.js
+++ next.config.js
@@ -1,13 +1,28 @@
-const { i18n } = require('./next-i18next.config');
+const { i18n } = require("./next-i18next.config");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  i18n,
-  images: {
-    domains: ['dummyimage.com'],
-  }
-}
+    async headers() {
+        return [
+            {
+                source: "/:all*(svg|jpg|png)",
+                locale: false,
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "public, max-age=9999999999, must-revalidate",
+                    },
+                ],
+            },
+        ];
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+    i18n,
+    images: {
+        domains: ["dummyimage.com"],
+        minimumCacheTTL: 60,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
