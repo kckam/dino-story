f5e59c27f026a20c5c4937a7d8db1baf7f0254f2
Kok Chin <kamkokchin@gmail.com>
2022-10-21 16:39:06 +0800

    up ip

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    const forwarded = req.headers["x-forwarded-for"];
+
+    const ip =
+        typeof forwarded === "string"
+            ? forwarded.split(/, /)[0]
+            : req.socket.remoteAddress;
+
+    console.log(ip);
+    res.status(200).json({ ip });
 }
